1756cfd6ea3a61632bf484535f9472ce5669d339
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-18 16:56:13 +0530

    [Aryan]Add. kilometer to miles conversion

---
+++ core-java-practice/gcr-codebase/programming-elements/kmtoMiles.java
@@ -0,0 +1,13 @@
+package gcr_codebase;
+
+public class kmtoMiles {
+
+	public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km * 0.621371; // conversion factor
+        
+        System.out.print ("Distance of "+ km + " km in miles is "+ miles);
+        
+    }
+
+}
