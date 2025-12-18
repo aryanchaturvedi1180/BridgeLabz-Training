7c7771565c60a730f3448e1b51494e6fa2ab8d3a
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-18 16:55:42 +0530

    [Aryan]Add. Percentage of 3 numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/FindPercentage.java
@@ -0,0 +1,16 @@
+package gcr_codebase;
+
+public class FindPercentage {
+	public static void main(String[] args) {
+		int math_marks = 94;
+		int chem_marks = 96;
+		int physics_marks = 95;
+		
+		float percentage = (chem_marks + math_marks + physics_marks) / 3; //percentage
+		
+		System.out.print ("Sam's average mark in PCM is " + percentage);
+				
+	    
+	}
+
+}
