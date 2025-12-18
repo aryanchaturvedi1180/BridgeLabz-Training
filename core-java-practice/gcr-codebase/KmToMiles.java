914a8ac0f752c87dc9886a31173762c624b6e529
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-18 16:20:41 +0530

    [Aryan]Add. Inital Commit

--- core-java-practice/gcr-codebase/KmToMiles.java
+++
@@ -1,8 +0,0 @@
-public class KmToMiles {
-    public static void main(String[] args) {
-        double km = 10.0;
-        double miles = km * 0.621371;
-        System.out.println("Distance in miles " + miles);
-    }
-    
-}
