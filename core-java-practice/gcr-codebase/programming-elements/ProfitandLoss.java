6362945218dfa306937b7122ed9fe515eefc004a
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-19 11:13:40 +0530

    [Aryan]Refactor. Profit and Loss

--- core-java-practice/gcr-codebase/programming-elements/ProfitandLoss.java
+++ core-java-practice/gcr-codebase/programming-elements/ProfitandLoss.java
@@ -2,13 +2,13 @@ package gcr_codebase;
 
 public class ProfitandLoss {
 	public static void main(String[] args) {
-		double CP = 129;
-        double SP = 70.0;
-        double profit = SP - CP;
-        double profitpercentage = (profit / CP )* 100; //formula
+		double costPrice = 129;
+        double sellingProfit = 70.0;
+        double profit = sellingProfit - costPrice;
+        double profitpercentage = (profit / costPrice )* 100; //formula
         
         
-        System.out.println("The Cost Price in INR " + CP + " and Selling Price in INR "+ SP);
+        System.out.println("The Cost Price in INR " + costPrice + " and Selling Price in INR "+ sellingProfit);
         System.out.println("Profit in INR " + profit + " and the Profit Percentage is " + profitpercentage);;
 	}
 
