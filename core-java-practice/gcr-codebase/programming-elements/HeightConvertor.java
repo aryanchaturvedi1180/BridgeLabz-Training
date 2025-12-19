5483d566f95085180e17a5ccb7d8f99702d76ac0
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-19 11:12:09 +0530

    [Aryan]Refactor. Height Conversion

--- core-java-practice/gcr-codebase/programming-elements/HeightConvertor.java
+++ core-java-practice/gcr-codebase/programming-elements/HeightConvertor.java
@@ -5,12 +5,12 @@ public class HeightConvertor {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter height in cm ");
-		double height_in_cm = input.nextDouble();
+		double heightInCm = input.nextDouble();
 		
-		double inches = height_in_cm / 2.54;
+		double inches = heightInCm / 2.54;
         double feet = inches / 12;
         
-        System.out.println("Your Height in cm is " + height_in_cm + "while in feet is " + feet + " and in inches is " + inches);
+        System.out.println("Your Height in cm is " + heightInCm + "while in feet is " + feet + " and in inches is " + inches);
         input.close();
 	}
 }
