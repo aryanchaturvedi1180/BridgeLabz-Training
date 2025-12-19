4ed6eb6d956ae14d880aec5db6669c60dbebb5a5
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-19 21:11:08 +0530

    [Aryan] Add.Greatest Factor Using For Loop

---
+++ core-java-practice/gcr-codebase/control-flow/GreatestFactorUsingFor.java
@@ -0,0 +1,23 @@
+package control_flow;
+
+import java.util.Scanner;
+
+public class GreatestFactorUsingFor {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+
+		int greatestFactor = 1;
+
+		for (int i = number - 1; i >= 1; i--) {
+			if (number % i == 0) {
+				greatestFactor = i;
+				break;
+			}
+		}
+
+		System.out.println(greatestFactor);
+		input.close();
+	}
+
+}
