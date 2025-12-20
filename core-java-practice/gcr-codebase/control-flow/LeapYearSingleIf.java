bdbd1106de178d3211996b4d7bcc3926975daa65
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-20 11:16:47 +0530

    [Aryan] Add.Leap Year Using Single If

---
+++ core-java-practice/gcr-codebase/control-flow/LeapYearSingleIf.java
@@ -0,0 +1,20 @@
+package control_flow;
+
+import java.util.*;
+
+public class LeapYearSingleIf {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int year = input.nextInt();
+
+		if (year >= 1582 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+			System.out.println("Leap Year");
+		} 
+		else {
+			System.out.println("Not a Leap Year");
+		}
+
+		input.close();
+	}
+}
