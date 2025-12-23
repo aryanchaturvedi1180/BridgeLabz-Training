cf9a6fb030975886e547b8e30c983b85e9763c91
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-23 12:33:32 +0530

    [Aryan] Add.First Non Repeating Character

---
+++ core-java-practice/gcr-codebase/strings/FirstNonRepeatingCharacter.java
@@ -0,0 +1,37 @@
+package strings;
+
+import java.util.*;
+
+public class FirstNonRepeatingCharacter {
+
+    static char findFirstNonRepeating(String text) {
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < text.length(); i++) {
+            freq[text.charAt(i)]++;
+        }
+
+        for (int i = 0; i < text.length(); i++) {
+            if (freq[text.charAt(i)] == 1) {
+                return text.charAt(i);
+            }
+        }
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+
+        char result = findFirstNonRepeating(text);
+
+        if (result != '\0')
+            System.out.println(result);
+        else
+            System.out.println("No Non-Repeating Character");
+        
+        input.close();
+    }
+}
