767af60ad4732ba2cc5119b8222ce02eb45e88ab
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2025-12-30 23:46:18 +0530

    [Aryan] Add.Item

---
+++ oops-practice/gcr-codebase/class-and-object/Item.java
@@ -0,0 +1,34 @@
+package classandobject;
+class Item {
+    String itemCode;
+    String itemName;
+    double itemPrice;
+
+    void display() {
+        System.out.println("itemCode : " + itemCode);
+        System.out.println("itemPrice : " + itemPrice);
+        System.out.println("itemName : " + itemName);
+        System.out.println("----------------------------");
+    }
+
+    public static void main(String[] args) {
+        Item i1 = new Item();
+        i1.itemCode = "01AA";
+        i1.itemPrice = 500.0;
+        i1.itemName = "Water bottle";
+
+        Item i2 = new Item();
+        i2.itemCode = "01BB";
+        i2.itemPrice = 700.0;
+        i2.itemName = "Rice";
+
+        Item i3 = new Item();
+        i3.itemCode = "02AA";
+        i3.itemPrice = 400.0;
+        i3.itemName = "blackboard";
+
+        i1.display();
+        i2.display();
+        i3.display();
+    }
+}
