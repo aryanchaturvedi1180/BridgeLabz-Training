10a9f974435bbc94bd5b31b1af6c754a2dd5de16
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2026-01-22 11:17:56 +0530

    [Aryan] Add.Sliding Window Max

---
+++ dsa-practice/gcr-codebase/Stack-Queue-HashMap-Hashing/SlidingWindowMax.java
@@ -0,0 +1,34 @@
+package stack_queue_hashMap_hashing;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlidingWindowMax {
+
+	static void maxSlidingWindow(int[] arr, int k) {
+		Deque<Integer> dq = new LinkedList<>();
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+				dq.pollFirst();
+			}
+
+			while (!dq.isEmpty() && arr[dq.peekLast()] < arr[i]) {
+				dq.pollLast();
+			}
+
+			dq.offerLast(i);
+
+			if (i >= k - 1) {
+				System.out.print(arr[dq.peekFirst()] + " ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, -1, -3, 5, 3, 6, 7 };
+		int k = 3;
+
+		maxSlidingWindow(arr, k);
+	}
+}
