cc8c26d26d97c60f17f951c7bc1e9e5dcdea9d58
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2026-02-02 21:47:34 +0530

    [Aryan] Add.Annotations

---
+++ collections-practice/gcr-codebase/annotations/override/Animal.java
@@ -0,0 +1,8 @@
+package annotations.override;
+
+public class Animal {
+
+	public void makeSound() {
+		System.out.println("Animal makes a sound");
+	}
+}
---
+++ collections-practice/gcr-codebase/annotations/override/TestOverride.java
@@ -0,0 +1,9 @@
+package annotations.override;
+
+public class TestOverride {
+	public static void main(String[] args) {
+
+		Dog dog = new Dog();
+		dog.makeSound();
+	}
+}
