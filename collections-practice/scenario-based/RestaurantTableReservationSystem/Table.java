0a7ac83053db9d37c11ce8c7d76d411907defcea
aryanchaturvedi1180 <aryanchaturvedi1180@gmail.com>
2026-02-03 15:43:37 +0530

    [Aryan] Add.RestaurantTableReservationSystem

---
+++ collections-practice/scenario-based/RestaurantTableReservationSystem/Table.java
@@ -0,0 +1,12 @@
+package scenario.RestaurantTableReservationSystem;
+
+class Table {
+    int tableNumber;
+    int capacity;
+
+    Table(int tableNumber, int capacity) {
+        this.tableNumber = tableNumber;
+        this.capacity = capacity;
+    }
+}
+
